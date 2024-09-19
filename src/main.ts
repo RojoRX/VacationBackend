edd0dc1d555c23a064889f819f539cbf47b4c2d4
RojoRX <ericrojomustaine666@outlook.es>
2024-09-19 00:25:21 -0400

    feat(api): Added Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   console.log(`USE_MOCK: ${configService.get<string>('USE_MOCK')}`); // Verifica el valor aquí
-  await app.listen(3000);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Vacaciones Backend API')
+    .setDescription('The API Vacations description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.BACKEND_PORT || 4001);
 }
 bootstrap();
